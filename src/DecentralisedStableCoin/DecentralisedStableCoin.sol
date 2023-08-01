4ba019f2f3248a4fc96e46894e35c29c64fba20f
isaadabbasi <itssaadabbasi@gmail.com>
2023-08-01 19:05:05 +0500

    chore: arrange the codebase to fly

---
+++ src/DecentralisedStableCoin/DecentralisedStableCoin.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+/**
+ * @title Decentralised Stable Coin
+ * @author Saad Abbasi, @isaadabbasi
+ * @notice This is an ERC20 implementation of a Decentralised Stable Coin.
+ * Where: 
+ * -  The stablility is Relative/Anchored Stability comes from collatoralization of Eth and BTC.
+ * -  Its pegged to USD (1 DSC -> 1 USD)
+ * - Governed by a DAO/Contract called DSC Engine, which makes it algorithmic.
+ * 
+ * @notice This contract is for educational purposes only. 
+ */
+contract DecentralisedStableCoin {
+  constructor() {
+    
+  }
+}
---
+++ test/DecentralisedStableCoin/DecentralisedStableCoin.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import { Test } from "forge-std/Test.sol";
+import { console } from "forge-std/console.sol";
+
+contract DecentralisedStableCoinTest is Test {
+  function setUp() external view {
+    console.log('TODO: Implement this...');
+  }
+}
